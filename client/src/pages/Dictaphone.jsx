ac0fc6892bd988b0637d6b548fe5f4b5faed5266
atharvasankdm <atharva19sk@gmail.com>
2023-02-03 04:25:08 +0530

    adding dictatophonee

--- client/src/pages/Dictaphone.jsx
+++ client/src/pages/Dictaphone.jsx
@@ -19,22 +19,34 @@ const Dictaphone = () => {
   //   return <span>Browser doesn't support speech recognition.</span>;
   // }
   useEffect(() => {
-    if (transcript.includes("upload")) {
-      navigate("/upload");
-    } else if (transcript.includes("images")) {
+    if (transcript.includes("home") || transcript.includes("Home")) {
+      navigate("/home");
+    } else if (transcript.includes("Jobs") || transcript.includes("jobs")) {
       navigate("/images");
-    } else if (transcript.includes("Sachin")) {
-      console.log("Kyu hila daala na");
+    } else if (transcript.includes("blog") || transcript.includes("Blog")) {
+      navigate("blog");
+    } else if (
+      transcript.includes("upskill") ||
+      transcript.includes("train") ||
+      transcript.includes("learn") ||
+      transcript.includes("motivation")
+    ) {
+      navigate("/training");
     }
   }, [transcript]);
 
   return (
     <div>
       <p>Microphone: {listening ? "on" : "off"}</p>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>h1</p>
+      <button
+        onClick={SpeechRecognition.startListening}
+        className="btn btn-primary"
+      >
+        Start
+      </button>
+      {/* <button onClick={SpeechRecognition.stopListening}>Stop</button> */}
+      {/* <button onClick={resetTranscript}>Reset</button> */}
+
       <p>{transcript}</p>
     </div>
   );
